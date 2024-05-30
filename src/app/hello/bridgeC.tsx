fdfc0dca38902b4f2d0027e0d018638a7270f8b3
hzz780 <hzz780@qq.com>
2024-05-30 22:22:51 +0800

    feat: 🎸 update

--- src/app/hello/bridgeC.tsx
+++ src/app/hello/bridgeC.tsx
@@ -1,6 +1,9 @@
 'use client'
 import AElfBridge from 'aelf-bridge';
 import {useState} from 'react';
+import VConsole from 'vconsole';
+const vConsole = new VConsole();
+// or init with options
 export default function BridgePage() {
   const [bridgeInstance, setBridgeInstance] = useState<any>(null);
   return <>
@@ -8,36 +11,37 @@ export default function BridgePage() {
       throw Error('click to throw error');
     }}>Click</div>
     <div onClick={() => {
+      console.log('AElfBridge: ', AElfBridge);
       const bridgeInstance = new AElfBridge({
         timeout: 3000,
         // endpoint: 'https://tdvv-public-node.aelf.io',
       });
       bridgeInstance.connect().then((isConnected: boolean) => {
         console.log('isConnected', isConnected);
-        alert('isConnected: ' + isConnected);
         setBridgeInstance(bridgeInstance);
+      }).catch((err: any) => {
+        console.log('err:', err);
       });
     }}
     >Click to Login</div>
     <div
       onClick={() => {
-        if (typeof window !== 'undefined') {
-          if (!bridgeInstance) {
-            alert('Please login first');
-            return;
-          }
-          const tokenAddress = 'ELF_7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX_tDVV'; // 合约地址可通过零合约的`GetContractAddressByName`只读方法获取
-          bridgeInstance.chain.contractAt(tokenAddress).then(async (contract: any) => {
-            const tokenInfo = await contract.GetTokenInfo.call({symbol: 'ELF'});
-            const transactionId = await contract.Approve({
-              amount: "10000000000",
-              spender: "FveRXL9PgVhMkoDcPh9jCkjF8WxW2K2aA72xAx4ngPqYnpNVw",
-              symbol: "ELF",
-            });
-            console.log(transactionId);
-            alert('transactionId: ' + transactionId);
-          })
+        console.log('Hello world');
+        if (!bridgeInstance) {
+          console.log('Please login first');
+          return;
         }
+        const tokenAddress = 'ELF_7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX_tDVV'; // 合约地址可通过零合约的`GetContractAddressByName`只读方法获取
+        bridgeInstance.chain.contractAt(tokenAddress).then(async (contract: any) => {
+          const tokenInfo = await contract.GetTokenInfo.call({symbol: 'ELF'});
+          const transactionId = await contract.Approve({
+            amount: "10000000000",
+            spender: "FveRXL9PgVhMkoDcPh9jCkjF8WxW2K2aA72xAx4ngPqYnpNVw",
+            symbol: "ELF",
+          });
+          console.log(transactionId);
+          alert('transactionId: ' + transactionId);
+        })
       }}
     >
       Click to Approve
