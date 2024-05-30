7b3e833bfa32e8fd70888591a50dc71ac7cdcdef
hzz780 <hzz780@qq.com>
2024-05-30 23:44:27 +0800

    feat: 🎸 no end point v2

--- src/app/hello/bridgeC.tsx
+++ src/app/hello/bridgeC.tsx
@@ -37,15 +37,15 @@ export default function BridgePage() {
     </div>
     <div
       onClick={() => {
-        console.log('Hello world');
+        console.log('Hello world', 'no end point');
         if (!bridgeInstance) {
           console.log('Please login first');
           return;
         }
         const tokenAddress = 'ELF_7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX_tDVV'; // 合约地址可通过零合约的`GetContractAddressByName`只读方法获取
         bridgeInstance.chain.contractAt(tokenAddress).then(async (contract: any) => {
-          const tokenInfo = await contract.GetTokenInfo.call({symbol: 'ELF'});
-          console.log('tokenInfo: ', tokenInfo);
+          // const tokenInfo = await contract.GetTokenInfo.call({symbol: 'ELF'});
+          // console.log('tokenInfo: ', tokenInfo);
           const transactionId = await contract.Approve({
             amount: "10000000000",
             spender: "FveRXL9PgVhMkoDcPh9jCkjF8WxW2K2aA72xAx4ngPqYnpNVw",
