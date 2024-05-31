c0efffe42a37e75fe231904df8974f6fe16ecb48
hzz780 <hzz780@qq.com>
2024-05-31 11:19:24 +0800

    feat: 🎸 proposal demo

--- src/app/hello/bridgeC.tsx
+++ src/app/hello/bridgeC.tsx
@@ -14,7 +14,7 @@ export default function BridgePage() {
       console.log('AElfBridge: ', AElfBridge);
       const bridgeInstance = new AElfBridge({
         timeout: 3000,
-        // endpoint: 'https://tdvv-public-node.aelf.io',
+        endpoint: 'https://tdvv-public-node.aelf.io',
       });
       bridgeInstance.connect().then((isConnected: boolean) => {
         console.log('isConnected', isConnected);
@@ -56,7 +56,23 @@ export default function BridgePage() {
         })
       }}
     >
-      Click to Approve
+      Click to Approve， Token
+    </div>
+    <div
+      onClick={() => {
+        if (!bridgeInstance) {
+          console.log('Please login first');
+          return;
+        }
+        const proposalContract = '4SGo3CUj3PPh3hC5oXV83WodyUaPHuz4trLoSTGFnxe84nqNr'; // 合约地址可通过零合约的`GetContractAddressByName`只读方法获取
+        bridgeInstance.chain.contractAt(proposalContract).then(async (contract: any) => {
+          console.log('proposalContract: ', proposalContract);
+          const transactionId = await contract.Approve("9a0b558ac3e6d7e9c64c2bdc430045e317a2bb5c16dfa25dfc1de1586dfd998a");
+          console.log(transactionId);
+        })
+      }}
+    >
+      Click to Approve, Proposal
     </div>
   </>
 }
