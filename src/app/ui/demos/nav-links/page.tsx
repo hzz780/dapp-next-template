9a471b0ea473f2397e27768623ba2e706b0b1a1c
hzz780 <hzz780@qq.com>
2024-06-05 11:05:16 +0800

    feat: 🎸 useSearchParams() with Suspense

--- src/app/ui/demos/nav-links/page.tsx
+++ src/app/ui/demos/nav-links/page.tsx
@@ -1,13 +1,17 @@
-// need to turn nav-links.tsx into a Client Component.
+/**
+ * 1. need to turn nav-links.tsx into a Client Component.
+ * 2. with a Suspense boundary
+ * https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+ */
 'use client';
-
 import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react'
 
 export default function NavLinks() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  return <>
+  return <Suspense>
     <p>path: {pathname}</p>
     <p>searchParams: {searchParams}</p>
-  </>
+  </Suspense>
 }
