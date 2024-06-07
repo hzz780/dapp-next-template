518e188f1c63207c5de6606d9b369d18bfd84a7c
hzz780 <hzz780@qq.com>
2024-06-07 18:26:23 +0800

    ci: 🎡 update log

--- src/app/api/demos/file-upload/route.ts
+++ src/app/api/demos/file-upload/route.ts
@@ -4,14 +4,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
-  console.log('初始的formData: ', formData);
+  console.log('formData: ', formData);
 
   const file = formData.get("file") as File;
 
-  console.log('file解析: ', file);
+  console.log('file: ', file);
 
   const fileBuffer = await file.arrayBuffer();
-  console.log('arrayBuffer, Buffer.from(这个arrayBuffer): ', __dirname, Buffer.from(fileBuffer));
+  console.log('arrayBuffer, Buffer.from(arrayBuffer): ', __dirname, Buffer.from(fileBuffer));
   await writeFile(`${__dirname}/${file.name}233`, Buffer.from(fileBuffer));
 
   return new NextResponse("success", { status: 200 });
